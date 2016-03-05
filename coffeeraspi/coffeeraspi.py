ed26c2259e7483716306f6174083e524bab45b37
Zachary Stewart <zachary@zstewart.com>
2016-03-05 14:01:01 -0500

    started raspi code

---
+++ coffeeraspi/coffeeraspi.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import argparse
+import asyncio
+import json
+import socket
+import websockets
+
+async def contact_server():
+    async with websockets.connect(server) as sock:
+        await sock.send(json.dumps(dict(
+            message='Hello',
+            name=name,
+            id=None, # In theory we would provide a unique ID for each machine, but we only have one...
+        )))
+
+        resp = await sock.recv()
+        # Handle new response
+        
+
+def main():
+    asyncio.get_event_loop().run_until_complete(contact_server())
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Client for connecting to AWS')
+    parser.add_argument('server', help='The server to connect to')
+    parser.add_argument(
+        '-n', '--name', default=socket.gethostname(),
+        help='The name of this client coffee machine'
+    )
+
+    args = parser.parse_args()
+    server = args.server
+    name = args.name
+
+    main()
