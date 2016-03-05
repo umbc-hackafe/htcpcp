e45705b04804dd7dfd5db8e852c7e9ad20590971
Alexander Bauer <sasha@linux.com>
2016-03-05 18:03:31 -0500

    Write serial mocking code for teensy

--- coffeeraspi/teensy.py
+++ coffeeraspi/teensy.py
@@ -13,8 +13,8 @@ class Interface():
             'brew':             b'h',
             'addin_insert':     b'i'
             }
-    def __init__(self, device):
-        self.serial = serial.Serial()
+    def __init__(self, device, mock=False):
+        self.serial = serial.Serial() if not mock else SerialMock()
         self.serial.port = device
 
     def __enter__(self):
@@ -28,3 +28,36 @@ class Interface():
         self.serial.open()
     def close(self):
         self.serial.close()
+
+class SerialMock():
+    def __init__(self, port = None, *args, **kwargs):
+        self._opened = False
+        self.is_open = False
+
+        self.port = port
+    def __enter__(self):
+        self.open()
+        return self
+    def __exit__(self, exception_type, exception_val, exception_tb):
+        self.close()
+        return False # Don't suppress exceptions
+
+    def open(self):
+        if self._opened:
+            raise IOError('Tried to re-open a mocked socket');
+        else:
+            self._opened = True
+            self.is_open = True
+
+    def close(self):
+        self.is_open = False
+
+    def read(self, n = 1):
+        if self.is_open:
+            return bytes([0] * n)
+        else:
+            raise IOError('Tried to read from a closed mocked socket');
+
+    def write(self, b):
+        print('SerialMock \'{}\': {}'.format(self.port, b))
+        return len(b)
